01847bbe2ee098c362d1c29f198e60946dda5221
Gonzalo30205 <56177558+Gonzalo30205@users.noreply.github.com>
2026-02-26 13:30:25 +0100

    producto con jerarquía (la 4 es gerente)

--- p2_g5/Func 1/producto.php
+++ p2_g5/Func 1/producto.php
@@ -9,21 +9,23 @@ class Producto {
     public $stock;
     public $ofertado;
 
-    public function __construct($id, $id_categoria, $nombre, $precio_base, $iva, $stock, $ofertado = 1) {
+    public function __construct($id, $id_categoria, $nombre, $descripcion, $precio_base, $iva, $stock, $ofertado = 1) {
         $this->id = $id;
         $this->id_categoria = $id_categoria;
         $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
         $this->precio_base = $precio_base;
         $this->iva = $iva;
         $this->stock = $stock;
         $this->ofertado = $ofertado;
     }
 
-    // Lógica requerida por el guion para el cálculo del precio final
+    // Cálculo del precio final según el guion
     public function getPrecioFinal() {
         return $this->precio_base * (1 + ($this->iva / 100));
     }
 
+    // Verifica disponibilidad (Stock y Borrado Lógico)
     public function estaDisponible() {
         return $this->stock > 0 && $this->ofertado == 1;
     }
