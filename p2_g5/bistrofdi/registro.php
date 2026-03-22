d2fe30b602a79d0f2ab595e57e5a06f509eaff48
RMGonzalo <60696655+RMGonzalo@users.noreply.github.com>
2026-03-22 21:41:06 +0100

    Cambios por hacer Feedback P2

--- p2_g5/bistrofdi/registro.php
+++ p2_g5/bistrofdi/registro.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/includes/sesion.php';
+require_once __DIR__ . '/includes/sesion.php'; 
 require_once __DIR__ . '/includes/funcionesUsuarios.php';
 
 $mensajeError = '';
