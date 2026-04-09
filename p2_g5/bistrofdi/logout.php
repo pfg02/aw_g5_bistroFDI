a65a126cf9d9f688d41f8a5c46bd2430295ff1f2
Gonzalo30205 <56177558+Gonzalo30205@users.noreply.github.com>
2026-04-09 22:08:23 +0200

    logout restablecido

--- p2_g5/bistrofdi/logout.php
+++ p2_g5/bistrofdi/logout.php
@@ -1,11 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/sesion.php';
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('Location: index.php');
-    exit;
-}
-
 logoutUsuario();
 header('Location: index.php');
 exit;
