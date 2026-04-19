767a08b44cfc38eb229f8a3803f9e6948b621457
Gonzalo30205 <56177558+Gonzalo30205@users.noreply.github.com>
2026-04-19 20:48:59 +0200

    Restructuración limpiando raíz completada

--- p3_g5/bistrofdi/includes/vistas/comun/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer-principal">
-    <p>&copy; <?php echo date('Y'); ?> Bistró FDI - Facultad de Informática</p>
-</footer>
--- p3_g5/bistrofdi/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/sesion.php';
-
-logoutUsuario();
-header('Location: index.php');
-exit;
