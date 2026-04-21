7c3c0eca7f7d90fd07fcc72184f6f9095732567c
Gonzalo30205 <56177558+Gonzalo30205@users.noreply.github.com>
2026-04-21 14:23:42 +0200

    Cambios para respetar feedback P2

--- p3_g5/bistrofdi/includes/acciones/auth/logout.php
+++ p3_g5/bistrofdi/includes/acciones/auth/logout.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/../../core/sesion.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: ../../../index.php');
+    exit;
+}
+
 logoutUsuario();
 header('Location: ../../../index.php');
 exit;
